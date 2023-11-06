e6e8cd4e57ca99711807e5c83d05de7c50f1dd21
mehdikolyaee98 <145374741+mehdikolyaee98@users.noreply.github.com>
2023-11-06 16:47:41 +0330

    Update validatePhone.go
    0930 رو الان قبول میکنه

--- utils/validatePhone.go
+++ utils/validatePhone.go
@@ -2,7 +2,7 @@ package utils
 
 import "regexp"
 
-var phoneRegex string = `^09(1[0-9]|3[1-9]|2[1-9]|9[1-9]|0[1-9]|4[1-9])-?[0-9]{3}-?[0-9]{4}$`
+var phoneRegex string = `^09(1[0-9]|3[0-9]|2[1-9]|9[1-9]|0[1-9]|4[1-9])-?[0-9]{3}-?[0-9]{4}$`
 
 func ValidatePhoneNumber(phone string) bool {
     r, _ := regexp.Compile(phoneRegex)
